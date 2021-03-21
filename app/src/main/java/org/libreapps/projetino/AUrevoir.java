cf26fdf276c123989b9f2523ce86c28c8ab59df7
Pc <baptiste.caq@gmail.com>
2021-03-21 09:43:37 +0100

    test

---
+++ app/src/main/java/org/libreapps/projetino/AUrevoir.java
@@ -0,0 +1,14 @@
+package org.libreapps.projetino;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AUrevoir extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_a_urevoir);
+    }
+}
---
+++ app/src/main/java/org/libreapps/projetino/accueil.java
@@ -0,0 +1,15 @@
+package org.libreapps.projetino;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class accueil extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_accueil);
+        //dada
+    }
+}
