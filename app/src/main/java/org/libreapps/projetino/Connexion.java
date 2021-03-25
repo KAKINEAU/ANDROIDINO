0b92a3d415e4722b98f56415d73ddc9799c8202f
victo <victor.li@esme.fr>
2021-03-25 13:50:06 +0100

    Test 1

--- app/src/main/java/org/libreapps/projetino/Connexion.java
+++ app/src/main/java/org/libreapps/projetino/Connexion.java
@@ -2,13 +2,42 @@ package org.libreapps.projetino;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Connexion extends AppCompatActivity {
+    private Button Inscrip;
+    private Button Connexion;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_connexion);
+        Inscrip = findViewById(R.id.button3);
+        Inscrip =(Button) findViewById(R.id.button3);
+        Inscrip.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                openactivity_connexion();
+            }
+        });
+        Connexion = findViewById(R.id.button2);
+        Connexion = (Button) findViewById(R.id.button2);
+        Connexion.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                openMenu();
+            }
+        });
+    }
+    public void openactivity_connexion(){
+        Intent intent1 = new Intent(this, inscription.class);
+        startActivity(intent1);
+    }
+    public void openMenu(){
+        Intent intent2 = new Intent(this, Menu.class);
+        startActivity(intent2);
     }
 }
--- app/src/main/java/org/libreapps/projetino/Menu.java
+++ app/src/main/java/org/libreapps/projetino/Menu.java
@@ -2,13 +2,28 @@ package org.libreapps.projetino;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Menu extends AppCompatActivity {
-
+    private Button Deconnexion;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+        Deconnexion = findViewById(R.id.button9);
+        Deconnexion =(Button) findViewById(R.id.button9);
+        Deconnexion.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                opena_urevoir();
+            }
+        });
+    }
+    public void opena_urevoir(){
+        Intent intent3 = new Intent(this, AUrevoir.class);
+        startActivity(intent3);
     }
 }
