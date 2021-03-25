68859a27034df3ba6549862752cca6f4430b1425
victo <victor.li@esme.fr>
2021-03-25 13:46:40 +0100

    Test 1

--- app/src/main/java/org/libreapps/projetino/accueil.java
+++ app/src/main/java/org/libreapps/projetino/accueil.java
@@ -2,14 +2,30 @@ package org.libreapps.projetino;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class accueil extends AppCompatActivity {
-
+    private Button Suivant;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_accueil);
-        //dada
+
+        Suivant = findViewById(R.id.button);
+        Suivant =(Button) findViewById(R.id.button);
+        Suivant.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                openPage_Connection();
+            }
+        });
+    }
+
+    public void openPage_Connection(){
+        Intent intent = new Intent(this, Connexion.class);
+        startActivity(intent);
     }
 }
