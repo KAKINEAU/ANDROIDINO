88c7bbf607e6cd3a18226c5179ab8bfb362c9087
viviannanas <vivi-anne.le@esme.fr>
2021-03-27 23:56:38 +0100

    page mon_compte

---
+++ app/src/main/java/org/libreapps/projetino/Mon_Compte.java
@@ -0,0 +1,14 @@
+package org.libreapps.projetino;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Mon_Compte extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mon__compte);
+    }
+}
