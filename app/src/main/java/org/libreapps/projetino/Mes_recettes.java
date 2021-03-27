a826ecaec39ab273e4c20f5d50153bdcf6b412d0
Pc <baptiste.caq@gmail.com>
2021-03-27 19:28:59 +0100

    ajout de la page mes recettes (à améliorer)

---
+++ app/src/main/java/org/libreapps/projetino/Mes_recettes.java
@@ -0,0 +1,14 @@
+package org.libreapps.projetino;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Mes_recettes extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mes_recettes);
+    }
+}
