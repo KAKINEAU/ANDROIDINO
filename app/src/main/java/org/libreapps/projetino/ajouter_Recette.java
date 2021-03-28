7d0728fc8ab5af244fd1e0980b021893a8a1ade8
victo <victor.li@esme.fr>
2021-03-28 17:14:34 +0200

    ajout de la création de recettes (à améliorer) + Tokens (pour l'instant sans effets)

--- app/src/main/java/org/libreapps/projetino/ajouter_Recette.java
+++ app/src/main/java/org/libreapps/projetino/ajouter_Recette.java
@@ -10,5 +10,6 @@ public class ajouter_Recette extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ajouter__recette);
+        //hhh
     }
 }
