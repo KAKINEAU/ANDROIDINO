33807d83cca29fc0f6909818ca91be806af053d3
victo <victor.li@esme.fr>
2021-04-08 09:54:19 +0200

    Fiche Recette

--- app/src/main/java/org/libreapps/projetino/Decouvrir.java
+++ app/src/main/java/org/libreapps/projetino/Decouvrir.java
@@ -2,7 +2,11 @@ package org.libreapps.projetino;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class Decouvrir extends AppCompatActivity {
 
@@ -10,5 +14,17 @@ public class Decouvrir extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_decouvrir);
+
+        ImageButton Boutonimage = findViewById(R.id.imageButton4);
+
+        Boutonimage.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Decouvrir.this, Fiche_Recette.class);
+                startActivity(intent);
+            }
+        });
     }
+
+
 }
--- app/src/main/java/org/libreapps/projetino/Mon_Compte.java
+++ app/src/main/java/org/libreapps/projetino/Mon_Compte.java
@@ -18,10 +18,15 @@ public class Mon_Compte extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mon__compte);
+        /*
         String nom_utilisateur = getIntent().getStringExtra("name");
+
         nom_Compte = (TextView) findViewById(R.id.nom_compte);
+
         nom_Compte.setText(nom_utilisateur);
+
         token = getIntent().getStringExtra("token");
+        */
         //transition page de déconnexion
         btthome = findViewById(R.id.button4);
         btthome.setOnClickListener(new View.OnClickListener(){
