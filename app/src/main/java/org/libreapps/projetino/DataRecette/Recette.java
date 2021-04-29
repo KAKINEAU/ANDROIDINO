6bf6dec62aa39f64acc80b29c5e3ec252dc7aaad
victo <victor.li@esme.fr>
2021-04-29 09:43:21 +0200

    affichage dans la page fiche recette qui fonctinne pas

--- app/src/main/java/org/libreapps/projetino/DataRecette/Recette.java
+++ app/src/main/java/org/libreapps/projetino/DataRecette/Recette.java
@@ -3,15 +3,18 @@ package org.libreapps.projetino.DataRecette;
 import org.json.JSONObject;
 
 public class Recette {
+    private final int id;
     private final String temps;
     private final String auteur;
 
     public Recette(JSONObject jObject) {
         this.temps = jObject.optString("temps");
         this.auteur = jObject.optString("auteur");
+        this.id = jObject.optInt("id");
 
     }
 
     public String getTemps(){ return temps;}
     public String getAuteur(){ return auteur;}
+    public int getId(){ return id; }
 }
--- app/src/main/java/org/libreapps/projetino/Mes_recettes.java
+++ app/src/main/java/org/libreapps/projetino/Mes_recettes.java
@@ -7,17 +7,49 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.TextView;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.libreapps.projetino.DataRecette.Recette;
+
+import java.util.ArrayList;
+import java.util.concurrent.ExecutionException;
+
 public class Mes_recettes extends AppCompatActivity {
     String token;
+    ConnectionRest connectionRest;
+    private ArrayList<Recette> listRecettes = new ArrayList<Recette>();
+
+    TextView auteur = (TextView) findViewById(R.id.textView39);
+    TextView tempspreparation = (TextView) findViewById(R.id.textView40);
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         token = getIntent().getStringExtra("token");
         setContentView(R.layout.activity_mes_recettes);
         Log.v("token", " " + token);
+
+        try{
+            connectionRest = new ConnectionRest();
+            connectionRest.setToken(token);
+            connectionRest.setAction("recette");
+            connectionRest.execute("GET");
+            String listJsonObjs = connectionRest.get();
+            if(listJsonObjs != null) {
+                listRecettes = parse(listJsonObjs);
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+
 
 
 
@@ -32,15 +64,34 @@ public class Mes_recettes extends AppCompatActivity {
         });
 
         ImageButton recette_N = findViewById(R.id.imageButton8);
+
         recette_N.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
                 Intent intent = new Intent(Mes_recettes.this,Fiche_Recette.class);
                 intent.putExtra("token", token);
+
                 startActivity(intent);
 
 
             }
         });
+    }
+
+    public ArrayList<Recette> parse(final String json) {
+        try {
+            final ArrayList<Recette> products = new ArrayList<>();
+            final JSONArray jProductArray = new JSONArray(json);
+            for (int i = 0; i < jProductArray.length(); i++) {
+                products.add(new Recette(jProductArray.optJSONObject(i)));
+                Log.v("TAG","[Auteur] : " + products.get(i).getAuteur()+" "+products.get(i).getId());
+                auteur.setText(products.get(i).getAuteur());
+            }
+            return products;
+        } catch (JSONException e) {
+            Log.v("TAG","[JSONException] e : " + e.getMessage());
+        }
+        return null;
     }
 }
